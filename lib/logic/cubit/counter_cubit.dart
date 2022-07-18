eb0e6ec9dd45ca0c6d6472f6caf22b4dccc22724
Tanmoy Sarkar <ts741127@gmail.com>
2022-07-19 04:11:25 +0530

    Bloc to bloc communication via bloc-listener

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -9,24 +9,9 @@ import '../../constants/enums.dart';
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  final InternetCubit internetCubit;
-  late StreamSubscription internetStreamSubscription;
 
-  CounterCubit({required this.internetCubit}) : super(CounterState(count: 0)) {
-    monitorInternetConnection();
-  }
+  CounterCubit() : super(CounterState(count: 0));
 
-  StreamSubscription<InternetState> monitorInternetConnection() {
-    return internetStreamSubscription = internetCubit.stream.listen((internetState) {
-    if (internetState is InternetConnected &&
-        internetState.connectionType == ConnectionType.Wifi) {
-      increment();
-    } else if (internetState is InternetConnected &&
-        internetState.connectionType == ConnectionType.Mobile) {
-      decrement();
-    }
-  });
-  }
 
   void increment() =>
       emit(CounterState(count: state.count + 1, wasIncremented: true));
@@ -34,7 +19,6 @@ class CounterCubit extends Cubit<CounterState> {
 
   @override
   Future<void> close() {
-    internetStreamSubscription.cancel();
     return super.close();
   }
 }
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learn_bloc/constants/enums.dart';
 import 'package:learn_bloc/logic/cubit/internet_cubit.dart';
 import 'package:learn_bloc/presentation/screens/second_screen.dart';
 
@@ -17,91 +18,101 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     print("BUILD HUHU");
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            BlocBuilder<InternetCubit, InternetState>(
-              builder: (context, state){
-                if(state is InternetConnected){
+    return BlocListener<InternetCubit, InternetState>(
+      listener: (context, state) {
+        if(state is InternetConnected){
+          if(state.connectionType == ConnectionType.Wifi){
+            BlocProvider.of<CounterCubit>(context).increment();
+          }else if(state.connectionType == ConnectionType.Mobile){
+            BlocProvider.of<CounterCubit>(context).decrement();
+          }
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              BlocBuilder<InternetCubit, InternetState>(
+                  builder: (context, state) {
+                if (state is InternetConnected) {
                   return Text("Connected to ${state.connectionType}");
-                }else if(state is InternetDisconnected){
+                } else if (state is InternetDisconnected) {
                   return Text("Disconnected");
-                }else{
+                } else {
                   return Text("Loading");
                 }
-              }
-            ),
-            const Text(
-              'COUNTER VALUE:',
-            ),
-            BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) => {
-                if (state.wasIncremented == true)
-                  {
-                    ScaffoldMessenger.of(context)
-                      ..hideCurrentSnackBar()
-                      ..showSnackBar(
-                        const SnackBar(
-                          content: Text('Counter incremented'),
-                        ),
-                      )
-                  }
-              },
-              builder: (context, state) {
-                return Text(
-                  state.count.toString(),
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
-                    // context.bloc<CounterCubit>().decrement();
-                  },
-                  tooltip: "Decrement",
-                  heroTag: "1",
-                  child: Icon(Icons.remove),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                  },
-                  tooltip: "Increment",
-                  heroTag: "2",
-                  child: Icon(Icons.add),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            MaterialButton(
-              color: Colors.green,
-              onPressed: () {
-                Navigator.of(context).pushNamed("/second");
-              },
-              child: Text("Go to screen 2"),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            MaterialButton(
-              color: Colors.red,
-              onPressed: () {
-                Navigator.of(context).pushNamed("/third");
-              },
-              child: Text("Go to screen 3"),
-            ),
-          ],
+              }),
+              const Text(
+                'COUNTER VALUE:',
+              ),
+              BlocConsumer<CounterCubit, CounterState>(
+                listener: (context, state) => {
+                  if (state.wasIncremented == true)
+                    {
+                      ScaffoldMessenger.of(context)
+                        ..hideCurrentSnackBar()
+                        ..showSnackBar(
+                          const SnackBar(
+                            content: Text('Counter incremented'),
+                          ),
+                        )
+                    }
+                },
+                builder: (context, state) {
+                  return Text(
+                    state.count.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                      // context.bloc<CounterCubit>().decrement();
+                    },
+                    tooltip: "Decrement",
+                    heroTag: "1",
+                    child: Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increment();
+                    },
+                    tooltip: "Increment",
+                    heroTag: "2",
+                    child: Icon(Icons.add),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              MaterialButton(
+                color: Colors.green,
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/second");
+                },
+                child: Text("Go to screen 2"),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              MaterialButton(
+                color: Colors.red,
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/third");
+                },
+                child: Text("Go to screen 3"),
+              ),
+            ],
+          ),
         ),
       ),
     );
